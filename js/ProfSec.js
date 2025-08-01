10e2659bddb20049b6c13fb08518e90a6aebc364
theFreePay <thefreepay.io@gmail.com>
2025-08-01 19:30:23 +0330

    Update ProfSec.js

--- js/ProfSec.js
+++ js/ProfSec.js
@@ -3833,7 +3833,7 @@ const supabase = window.supabase.createClient(
   
         
         
-        window.richadsController.triggerInterstitialVideo().then((result) => {
+        richadsController.triggerInterstitialVideo().then((result) => {
           // Ok 
           async function secsess() {
             ExValue++
